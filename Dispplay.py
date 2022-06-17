51f5754b1510e33d0dd7a80e6e3c7fef0844cb9f
Tushar <tusharpradhan402@gmail.com>
2022-06-17 18:51:38 +0530

    display file added with new function

--- Dispplay.py
+++ Dispplay.py
@@ -5,7 +5,13 @@ class Demo:
 
 
         print("new display:",self.a)
+        print("this is added in this release")
+
+    def Dis(self):
+
+        print("new function added")
 
 
 obj=Demo()
 obj.Add()
+obj.Dis()
